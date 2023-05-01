d10806ab1cff404291c6747f41ed394e47cb9579
unknown <partharora557@gmail.com>
2023-05-01 20:31:05 +0530

    init

--- src/components/ApiContainer/index.jsx
+++ src/components/ApiContainer/index.jsx
@@ -2,40 +2,63 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const ApiContainer = () => {
-  const [input, setInput] = useState();
+  const [input, setInput] = useState("");
+  const [images, setImages] = useState();
+  const [changing, setChanging] = useState();
 
-  let url = "https://dagk2i0l0yytq4-7860.proxy.runpod.net/sdapi/v1/txt2img/";
+  const [loading, setLoading] = useState(false);
 
   React.useEffect(() => {
-    const submit = async () => {
-      console.log(input);
+    const catchIt = () => {
+      setLoading(true);
+      const url =
+        "https://dagk2i0l0yytq4-7861.proxy.runpod.net/sdapi/v1/txt2img";
+
+      const requestData = {
+        prompt: input,
+        steps: 30,
+      };
+
+      const config = {
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json",
+          mode: "no-cors",
+        },
+      };
+
       axios
-        .post(url, {
-          prompt: "gray colored",
-          steps: 30,
+        .post(url, requestData, config)
+        .then((response) => {
+          console.log(response.data);
+          const image = response.data.images[0];
+          const imageBytes = atob(image);
+          const imageData = new Uint8Array(imageBytes.length);
+          for (let i = 0; i < imageBytes.length; i++) {
+            imageData[i] = imageBytes.charCodeAt(i);
+          }
+          const blob = new Blob([imageData], { type: "image/png" });
+          const imageUrl = URL.createObjectURL(blob);
+          const img = new Image();
+          img.onload = () => {
+            URL.revokeObjectURL(imageUrl);
+          };
+          img.src = imageUrl;
+          console.log(img);
+          console.log(imageUrl);
+          setImages(imageUrl);
+          setLoading(false);
         })
-        .then((e) => {
-          console.log(e);
+        .catch((error) => {
+          console.error(error);
         });
     };
-    const payload = {
-      prompt: "gray colored",
-      steps: 30,
-    };
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify(payload),
-      mode: "no-cors",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        const image = data.images[0];
-        console.log(image);
-        // Do something with the image...
-      })
-      .catch((error) => console.error(error));
-    // submit();
-  });
+    if (input.length !== 0) {
+      catchIt();
+    }
+  }, [changing]);
+
+  console.log(images);
   return (
     <div className="w-[100%] flex items-center flex-col justify-center">
       <input
@@ -48,29 +71,36 @@ const ApiContainer = () => {
         className="w-[400px] h-[44px] px-[10px] text-[#000] font-medium rounded-sm mt-[40px] border-[2px] outline-none border-[#222]"
       />
       <button
-        // onClick={() => {
-        //   submit();
-        // }}
+        onClick={() => {
+          setChanging(!changing);
+        }}
         className="w-[400px] h-[45px] rounded-sm hover:bg-[#444] bg-[#222] text-[#fff] flex items-center justify-center text-[15px] font-medium mt-[10px]"
       >
-        Generate
+        {loading ? "Generating..." : "Generate"}
       </button>
       <div className="w-[400px] h-[350px] mt-[20px] border-[1.5px] rounded-sm border-[#000]   p-[10px] flex-col flex items-center">
-        <h4 className="text-[#000] font-medium text-center uppercase tracking-wide font-mono">
+        <h4 className="text-[#000] h-[50px] font-medium text-center uppercase tracking-wide font-mono">
           Result
         </h4>
-        <div className="mt-[5px] rounded-sm w-[100%] h-[100%] border-[1.5px] border-[#000] flex items-center justify-center p-[10px]">
-          <div className="flex items-center justify-center">
+        <div className="mt-[5px] rounded-sm w-[100%] h-[300px] border-[1.5px] border-[#000] flex items-center justify-center p-[10px]">
+          {images ? (
             <img
-              src="/image.svg"
-              className="w-[24px] h-[24px] mr-[5px] translate-y-[1px]"
+              src={images}
+              className="w-[100%] h-[100%] object-contain "
               alt=""
             />
-            <p className="tracking-wide text-[#000] text-[17px] font-medium">
-              {" "}
-              Images will Appear here
-            </p>
-          </div>
+          ) : (
+            <div className="flex items-center justify-center">
+              <img
+                src="/image.svg"
+                className="w-[24px] h-[24px] mr-[5px] translate-y-[1px]"
+                alt=""
+              />
+              <p className="tracking-wide text-[#000] text-[17px] font-medium">
+                Images will Appear here
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- src/views/index.jsx
+++ src/views/index.jsx
@@ -3,7 +3,7 @@ import ApiContainer from "../components/ApiContainer";
 
 const Home = () => {
   return (
-    <div className="w-[100%] h-[100vh] bg-[#fff] ">
+    <div className="w-[100%] h-[100vh] bg-[#fff]  overflow-hidden">
       <h1 className="text-[40px] text-[#000] font-mono font-medium text-center mt-[100px] tracking-wide">
         Stable Diffusion Api
       </h1>
