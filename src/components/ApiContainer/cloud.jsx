71aa93318f42c93a8a7b777132f08ae22d1130c9
unknown <partharora557@gmail.com>
2023-05-03 20:07:36 +0530

    done

---
+++ src/components/ApiContainer/cloud.jsx
@@ -0,0 +1,80 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const CloudContainer = ({ base }) => {
+  const [input, setInput] = useState("");
+  const [changing, setChanging] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [check, setCheck] = useState(false);
+
+  const [link, setLink] = useState("");
+
+  function generateUniqueId() {
+    const timestamp = Date.now().toString(36); // Convert current timestamp to base36 string
+    const randomNumber = Math.random().toString(36).substring(2, 8); // Generate random base36 string
+    const uniqueId = `${timestamp}-${randomNumber}`; // Combine timestamp and random number
+    return uniqueId;
+  }
+  console.log(generateUniqueId());
+  const catchIt1 = (e) => {
+    const formDate = new FormData();
+    formDate.append("title", e);
+
+    const requestData = formDate;
+    const config = {
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    };
+    const url = "http://localhost:8080/feed/post";
+    console.log(e, requestData);
+    axios.post(url, requestData, config).then((e) => {
+      setLink(e.data.post.image);
+    });
+  };
+  console.log(link);
+  React.useEffect(() => {
+    if (check) {
+      if (base.length !== 0) {
+        setLoading(true);
+        catchIt1(base);
+        setLoading(false);
+      }
+    }
+    setCheck(true);
+  }, [changing]);
+
+  return (
+    <div className="w-[50%] flex flex-col items-center">
+      <h1 className="text-[40px] w-[80%] text-[#000] font-mono font-medium text-center mt-[100px] tracking-wide">
+        Send the Image to cloud!
+      </h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+        placeholder="name for the Image?"
+        className="w-[400px] h-[44px] px-[10px] text-[#000] font-medium rounded-sm mt-[40px] border-[2px] outline-none border-[#222]"
+      />
+      <button
+        onClick={() => {
+          setChanging(!changing);
+        }}
+        className="w-[400px] h-[45px] rounded-sm hover:bg-[#444] bg-[#222] text-[#fff] flex items-center justify-center text-[15px] font-medium mt-[10px]"
+      >
+        {loading ? "Sending..." : "Send"}
+      </button>
+      {link.length !== 0 && (
+        <>
+        <h4 className="font-bold text-[20px] text-[#000] mt-[40px] w-[70%]" >Link:</h4>
+        <a href={link} target="_blank" className="w-[70%] shrink-0 text-[#3333FF] cursor-pointer mt-[10px]  text-[16px] font-medium">{link}</a>
+        </>
+      )}  
+    </div>
+  );
+};
+
+export default CloudContainer;
--- src/components/ApiContainer/index.jsx
+++ src/components/ApiContainer/index.jsx
@@ -1,25 +1,36 @@
 import React, { useState } from "react";
 import axios from "axios";
+import CloudContainer from "./cloud";
 
 const ApiContainer = () => {
   const [input, setInput] = useState("");
   const [images, setImages] = useState();
   const [changing, setChanging] = useState();
-
+  const [basee, setBasee] = useState("");
   const [loading, setLoading] = useState(false);
 
-  React.useEffect(() => {
-    const catchAnother = () => {
-      const url = "https://localhost:8080/feed/posts";
-      axios.get(url).then((e) => {
-        console.log(e, "come on get it!!");
+  const catchAnother = async (blobUrl) => {
+    fetch(blobUrl)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onloadend = function () {
+          const base64data = reader.result;
+          console.log(base64data);
+          setBasee(base64data);
+        };
+      })
+      .catch((error) => {
+        console.error("There was an error:", error);
       });
-    };
-    catchAnother();
+  };
+
+  React.useEffect(() => {
     const catchIt = () => {
       setLoading(true);
       const url =
-        "https://dagk2i0l0yytq4-7861.proxy.runpod.net/sdapi/v1/txt2img";
+        "https://i44qfg7p5z4yyn-7861.proxy.runpod.net/sdapi/v1/txt2img";
 
       const requestData = {
         prompt: input,
@@ -44,7 +55,7 @@ const ApiContainer = () => {
           for (let i = 0; i < imageBytes.length; i++) {
             imageData[i] = imageBytes.charCodeAt(i);
           }
-          const blob = new Blob([imageData], { type: "image/png" });
+          const blob = new Blob([imageData], { type: "image/jpg" });
           const imageUrl = URL.createObjectURL(blob);
           console.log(imageUrl);
           const img = new Image();
@@ -52,12 +63,14 @@ const ApiContainer = () => {
             URL.revokeObjectURL(imageUrl);
             console.log(imageUrl);
           };
-          fetch(imageUrl).then((e) => {
-            console.log(e, "pleaeefa");
-          });
           img.src = imageUrl;
-          console.log(img);
-          console.log(imageUrl);
+          // catchAnother(image);
+          // catchIt1(image)
+
+          // catchAnother(imageUrl);
+          // setBasee(imageUrl);
+          catchAnother(imageUrl);
+
           setImages(imageUrl);
           setLoading(false);
         })
@@ -70,51 +83,57 @@ const ApiContainer = () => {
     }
   }, [changing]);
 
-  console.log(images);
+  console.log(basee);
   return (
-    <div className="w-[100%] flex items-center flex-col justify-center">
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => {
-          setInput(e.target.value);
-        }}
-        placeholder="Tell me about the Image you desire?"
-        className="w-[400px] h-[44px] px-[10px] text-[#000] font-medium rounded-sm mt-[40px] border-[2px] outline-none border-[#222]"
-      />
-      <button
-        onClick={() => {
-          setChanging(!changing);
-        }}
-        className="w-[400px] h-[45px] rounded-sm hover:bg-[#444] bg-[#222] text-[#fff] flex items-center justify-center text-[15px] font-medium mt-[10px]"
-      >
-        {loading ? "Generating..." : "Generate"}
-      </button>
-      <div className="w-[400px] h-[350px] mt-[20px] border-[1.5px] rounded-sm border-[#000]   p-[10px] flex-col flex items-center">
-        <h4 className="text-[#000] h-[50px] font-medium text-center uppercase tracking-wide font-mono">
-          Result
-        </h4>
-        <div className="mt-[5px] rounded-sm w-[100%] h-[300px] border-[1.5px] border-[#000] flex items-center justify-center p-[10px]">
-          {images ? (
-            <img
-              src={images}
-              className="w-[100%] h-[100%] object-contain "
-              alt=""
-            />
-          ) : (
-            <div className="flex items-center justify-center">
+    <div className="w-[100%] flex">
+      <div className="w-[50%] flex items-center flex-col justify-center">
+        <h1 className="text-[40px] text-[#000] font-mono font-medium text-center mt-[100px] tracking-wide">
+          Stable Diffusion Api
+        </h1>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          placeholder="Tell me about the Image you desire?"
+          className="w-[400px] h-[44px] px-[10px] text-[#000] font-medium rounded-sm mt-[40px] border-[2px] outline-none border-[#222]"
+        />
+        <button
+          onClick={() => {
+            setChanging(!changing);
+          }}
+          className="w-[400px] h-[45px] rounded-sm hover:bg-[#444] bg-[#222] text-[#fff] flex items-center justify-center text-[15px] font-medium mt-[10px]"
+        >
+          {loading ? "Generating..." : "Generate"}
+        </button>
+        <div className="w-[400px] h-[350px] mt-[20px] border-[1.5px] rounded-sm border-[#000]   p-[10px] flex-col flex items-center">
+          <h4 className="text-[#000] h-[50px] font-medium text-center uppercase tracking-wide font-mono">
+            Result
+          </h4>
+          <div className="mt-[5px] rounded-sm w-[100%] h-[300px] border-[1.5px] border-[#000] flex items-center justify-center p-[10px]">
+            {images ? (
               <img
-                src="/image.svg"
-                className="w-[24px] h-[24px] mr-[5px] translate-y-[1px]"
+                src={images}
+                className="w-[100%] h-[100%] object-contain "
                 alt=""
               />
-              <p className="tracking-wide text-[#000] text-[17px] font-medium">
-                Images will Appear here
-              </p>
-            </div>
-          )}
+            ) : (
+              <div className="flex items-center justify-center">
+                <img
+                  src="/image.svg"
+                  className="w-[24px] h-[24px] mr-[5px] translate-y-[1px]"
+                  alt=""
+                />
+                <p className="tracking-wide text-[#000] text-[17px] font-medium">
+                  Images will Appear here
+                </p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
+      <CloudContainer base={basee} />
     </div>
   );
 };
